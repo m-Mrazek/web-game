8a06d4b1a94db8d320c259cb29e3b3d2dde3ec61
m-Mrazek <m.mrazek21@gmail.com>
2022-05-04 20:48:14 +0200

    added tilesets and initalized map

---
+++ views/tiled/FloorTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="WebGame3" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+ <image source="../../../../Users/benja/OneDrive/Desktop/Ninja Adventure/NinjaAdventure/Backgrounds/Tilesets/TilesetFloor.png" width="352" height="417"/>
+</tileset>
---
+++ views/tiled/HouseTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="HouseTiles" tilewidth="16" tileheight="16" tilecount="667" columns="29">
+ <image source="../../../../Users/benja/OneDrive/Desktop/Ninja Adventure/NinjaAdventure/Backgrounds/Tilesets/TilesetHouse.png" width="464" height="368"/>
+</tileset>
